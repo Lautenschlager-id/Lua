b8eb4b86bce9f8b5ad1ef4e4974f1680b34ec09b
Lautenschlager <tai.romani@hotmail.com>
2017-07-10 18:45:50 -0300

    Create aaaaa.lua

---
+++ Codification/aaaaa.lua
@@ -0,0 +1,65 @@
+serialization = function(i,lastIndex)
+	lastIndex = lastIndex or ""
+	if type(i) == "table" then
+		local out = ""
+	
+		for k,v in next,i do
+			if type(v) == "string" then
+				out = out .. string.format("@[%s][%s]",string.format("%s[%s]",lastIndex,k),table.concat({string.byte(v,1,#v)},","))
+			elseif type(v) == "number" then
+				out = out .. string.format("#[%s][%s]",string.format("%s[%s]",lastIndex,k),tostring(v))
+			elseif type(v) == "boolean" then
+				out = out .. string.format("![%s][%s]",string.format("%s[%s]",lastIndex,k),tostring(v))
+			elseif type(v) == "table" then
+				out = out .. serialization(v,string.format("%s[%s]",lastIndex,k))
+			end
+		end
+
+		return out
+	elseif type(i) == "string" then
+		local out = {}
+
+		for t,k,v in string.gmatch(i,"([@#!=])%[(.-)%]%[(.-)%]") do
+			local value
+			if t == "@" then
+				value = string.char((function(bytes)
+					local foo = {}
+					for byte in string.gmatch(bytes,"[^,]+") do
+						foo[#foo + 1] = tonumber(byte)
+					end
+					return table.unpack(foo)
+				end)(v))
+			elseif t == "#" then
+				value = tonumber(v)
+			elseif t == "!" then
+				value = (v == "true")
+			elseif t == "=" then
+				value = serialization(v)
+			end
+			
+			local index = {}
+			for j in string.gmatch(k,"[^%[%]]") do
+				index[#index + 1] = tonumber(j) or j
+			end
+
+			if #index == 1 then
+				out[index[1]] = value
+			else
+				local m,n = 1
+				while m <= #index do
+					if not n then
+						out[index[m]] = {}
+						n = out[index[m]]
+						m = m + 1
+					end
+					
+					n[index[m]] = (m == #index and value or {})
+					n = n[index[m]]
+					m = m + 1
+				end
+			end
+		end
+
+		return out
+	end
+end
